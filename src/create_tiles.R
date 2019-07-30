4cf57225ef34246fd4cc5c08c55df6a56230d191
projectflutrend <bitowaqr@gmail.com>
2019-07-30 18:02:23 +0100

    update

--- src/create_tiles.R
+++ src/create_tiles.R
@@ -1,18 +1,16 @@
 library(raster)
 library(tiler)
+
 tiler_options(python = "python3")
 tile_dir <- paste(getwd(),"/tiles/",sep="")
 
-# load(file.choose())
+lsoa_sp = shapefile(x = "./raw/lsoa_sp")
+event_sp = shapefile(x = "./raw/event_sp")
 
-# pal <- colorRampPalette(c("darkblue", "lightblue"))(20)
-# nodata <- "tomato"
-# tile(map, tile_dir, "0-3", col = pal, colNA = nodata)
 
-poly = event_sp
-r <- raster(ncol=300, nrow=300)
-extent(r) <- extent(poly)
-rp <- rasterize(poly, r, 1)
+raster_grid <- raster(ncol=500, nrow=500)
+extent(raster_grid) <- extent(lsoa_sp)
+rasterized_lsoa <- rasterize(lsoa_sp, r, "mn_dstn")
 plot(rp)
 
 writeRaster(rp, filename="test.tif", format="GTiff", overwrite=TRUE)
@@ -28,5 +26,11 @@ leaflet(options = leafletOptions(minZoom = 0, maxZoom = 7), width = "100%") %>%
   addProviderTiles("Stamen.Toner") %>% 
   addTiles(tiles, options = tileOptions(opacity = 1)) %>%
   setView(0, 52, zoom = 6) 
+
+
+
+# pal <- colorRampPalette(c("darkblue", "lightblue"))(20)
+# nodata <- "tomato"
+# tile(map, tile_dir, "0-3", col = pal, colNA = nodata)
 
 
