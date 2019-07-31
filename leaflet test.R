cfe2bff5fabf4e9711a0f6c82aeda51342fda232
projectflutrend <bitowaqr@gmail.com>
2019-07-31 20:02:35 +0100

    new tiles

---
+++ leaflet test.R
@@ -0,0 +1,54 @@
+
+
+
+event_sp = shapefile(x = "./raw/event_sp")
+library(leaflet)
+  
+  tiles_distance <- "https://bitowaqr.github.io/iol_map/tiles/{z}/{x}/{y}.png"
+  
+  leaflet(options = leafletOptions(minZoom = 4, maxZoom = 12), width = "100%") %>%
+  
+    
+    
+    # provider tile
+    addTiles(group="OSM") %>%
+    addProviderTiles(providers$CartoDB.Positron, group="Carto") %>% 
+    
+    
+    # my tile
+    addTiles(tiles_distance, options = tileOptions(opacity = 0.5),group="Distances") %>%
+    
+    
+    # established events
+    addCircleMarkers(
+      group = "Parkrun events",
+      data = event_sp,
+      # lng = ~lon,lat = ~lat,
+      radius = 3,fillColor = "blue",
+      stroke = F, fillOpacity = 1,
+      popup = paste("Course:",event_sp$course,"<br>",
+                    "Established:", event_sp$Estblsh,"<br>",
+                    "Served pop:", round(event_sp$srvd_pop),"<br>",
+                    "Served LSOA:", round(event_sp$srvd_lsoa),"<br>",
+                    "Mean participants:", round(event_sp$Mn_prtc),"<br>",
+                    "Mean volunteers:", round(event_sp$Mn_vlnt))
+    ) %>% 
+    addLegend(
+      group = "Distances",
+      colors =c("darkgreen","green","orange","red","darkred"),
+      title = "Distance to the <br> Nearest parkrun event",
+      labels = c("&nbsp < 1 km","  1-2.5 km","2.5- 5 km","&nbsp 5 -10 km","&nbsp   > 10 km"),
+      labFormat = labelFormat(prefix = "", suffix = "km", between = " &ndash; ",
+                              digits = 0, big.mark = ",", transform = identity)
+      ) %>%
+  
+  # leaflet options
+  setView(0, 52, zoom = 7) %>%
+    addLayersControl(
+      baseGroups = c("OSM","Carto"),
+      overlayGroups = c("Distances"),
+      options = layersControlOptions(collapsed = F,autoZIndex=T) 
+    ) %>%
+    hideGroup("Distances")
+  
+  
--- src/create_tiles.R
+++ src/create_tiles.R
@@ -2,6 +2,9 @@
 library(raster)
 library(tiler)
 library(leaflet)
+library(fasterize)
+library(sf)
+
 
 # set tiler options
 tiler_options(python = "python3")
@@ -9,33 +12,35 @@ tile_dir_mn_dstn <- paste(getwd(),"/tiles_distance/",sep="")
 tile_dir_participation <- paste(getwd(),"/tiles_participation/",sep="")
 
 # load data
-lsoa_sp = shapefile(x = "./raw/lsoa_sp")
+lsoa_sp = shapefile(x = "./raw/lsoa_sf")
+lsoa_sf = st_as_sf(lsoa_sp)
 
 # define raster grid
 raster_grid <- raster(ncol=5000, nrow=5000)
-extent(raster_grid) <- extent(lsoa_sp)
+extent(raster_grid) <- extent(lsoa_sf)
 
 # mn distance tile
-lsoa_sp$mn_dstn_cuts = cut(lsoa_sp$mn_dstn,breaks = c(min(lsoa_sp$mn_dstn),1,2.5,5,10,20,max(lsoa_sp$mn_dstn)))
-rasterized_lsoa_mn_dstn5 <- rasterize(lsoa_sp, raster_grid, "mn_dstn_cuts")
+lsoa_sf$mn_dstn_cuts = cut(lsoa_sf$mn_dstn,breaks = c(0,1,2.5,5,10,max(lsoa_sf$mn_dstn)))
+rasterized_lsoa_mn_dstn5 <- fasterize(lsoa_sf, raster_grid, "mn_dstn_cuts")
 writeRaster(rasterized_lsoa_mn_dstn5, filename="./raw/lsoa_mn_dstn.tif", format="GTiff", overwrite=TRUE)
 mn_dsnt_tif_path <- "./raw/lsoa_mn_dstn.tif"
-crs <- as.character(crs(rasterized_lsoa_mn_dstn5))
+crs_dist <- as.character(crs(rasterized_lsoa_mn_dstn5))
 pal <- colorRampPalette(c("darkgreen","green","orange","red","darkred"))(5)
 nodata <- "white"
-tile(mn_dsnt_tif_path, tile_dir_mn_dstn, "1-11", col = pal,crs = crs)
+tile(mn_dsnt_tif_path, tile_dir_mn_dstn, "4-12", col = pal,crs = crs_dist)
 rm("rasterized_lsoa_mn_dstn5")     
 
-# mn distance tile
-lsoa_sp$mn_dstn_cuts = cut(lsoa_sp$mn_dstn,breaks = c(min(lsoa_sp$mn_dstn),1,2.5,5,10,20,max(lsoa_sp$mn_dstn)))
-rasterized_lsoa_mn_dstn5 <- rasterize(lsoa_sp, raster_grid, "mn_dstn_cuts")
-writeRaster(rasterized_lsoa_mn_dstn5, filename="./raw/lsoa_mn_dstn.tif", format="GTiff", overwrite=TRUE)
-mn_dsnt_tif_path <- "./raw/lsoa_mn_dstn.tif"
-crs <- as.character(crs(rasterized_lsoa_mn_dstn5))
-pal <- colorRampPalette(c("darkgreen","green","orange","red","darkred"))(5)
+# participation tile
+# lsoa_sf$part_cuts = base::cut(lsoa_sf$rns_pm_,include.lowest = T,breaks = c(min(lsoa_sf$rns_pm_),0.1^5,0.5,1,2,max(lsoa_sf$rns_pm_)+1))
+lsoa_sf$part_cuts = base::cut(lsoa_sf$rns_pm_,include.lowest = T,breaks = c(-1,0,0.5,1,2,20))
+rasterized_lsoa_part5 <- rasterize(lsoa_sf, raster_grid, "part_cuts")
+writeRaster(rasterized_lsoa_part5, filename="./raw/lsoa_participation_5.tif", format="GTiff", overwrite=TRUE)
+part_tif_path <- "./raw/lsoa_participation_5.tif"
+crs <- as.character(crs(rasterized_lsoa_part5))
+pal_part <- colorRampPalette(c("darkred","red","orange","green","darkgreen"))(5)
 nodata <- "white"
-tile(mn_dsnt_tif_path, tile_dir_mn_dstn, "1-11", col = pal,crs = crs)
-
+tile(part_tif_path, tile_dir_participation, "4-12", col = pal_part,crs = crs)
+rm("rasterized_lsoa_part5")     
 
 
  # x <- RGB(rasterized_lsoa_mn_dstn5,col=colorRampPalette(c("darkgreen","yellow","greenyellow","gold","orange", "red","darkred"))(10))
